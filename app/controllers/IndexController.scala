b142d5d051ad4e380216f75be54a26e6c6ca969a
parkeradam <30392071+parkeradam@users.noreply.github.com>
2020-07-09 14:16:23 +0100

    Formatting updates.

--- app/controllers/IndexController.scala
+++ app/controllers/IndexController.scala
@@ -30,12 +30,12 @@ import uk.gov.hmrc.play.bootstrap.controller.FrontendBaseController
 import scala.concurrent.ExecutionContext
 
 class IndexController @Inject()(
-    val controllerComponents: MessagesControllerComponents,
-    renderer: Renderer,
-    navigator: Navigator,
-    identify: IdentifierAction,
-    getData: DataRetrievalAction,
-    sessionRepository: SessionRepository
+                                val controllerComponents: MessagesControllerComponents,
+                                renderer: Renderer,
+                                navigator: Navigator,
+                                identify: IdentifierAction,
+                                getData: DataRetrievalAction,
+                                sessionRepository: SessionRepository
 )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
 
   def onPageLoad: Action[AnyContent] = Action.async {
